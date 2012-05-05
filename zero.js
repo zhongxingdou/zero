fcdbe16b268d26a7a94ac7456bfd6459e49a2d0e
zhongxingdou <zhongxingdou@gmail.com>
2012-05-05 23:13:40 +0800

    移除$bindProperty()的代码

--- zero.js
+++ zero.js
@@ -13,12 +13,4 @@
 /**
  * 连接两个对象的属性，当其中一个改变时，更新另一个
  */
-function $bindProperty(obj, name, obj2, name2, bidirectional){
-	if($support(obj, IEvent)){
-		if(!name2)name2 = nameo		var upName2 = name2.slice(0,1).toUpperCase() + name2.slice(1);
-		obj.on(name + "Changed").then($call(obj2, "set" + upName2));
-		if(bidirectional){
-			$syncProperty(obj2, name2, obj, name);
-		}
-	}
-}
+function $bindProperty(obj, name, obj2, name2, bidirectional){}
