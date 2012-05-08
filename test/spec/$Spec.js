77615a2c9de1ddc9282381b9c70bb12f770317c9
zhongxingdou <zhongxingdou@gmail.com>
2012-05-09 03:14:56 +0800

    为$global的上一次提交的添加和更新测试

--- test/spec/$Spec.js
+++ test/spec/$Spec.js
@@ -1,14 +1,14 @@
 describe("$()", function() {
 	it("注册和获取wrapper", function() {
 		var w = function() {};
-		$.regist("number", w);
+		$.regist(w, "number");
 
 		expect($.getWrapper("number")).toBe(w);
 	});
 
 	it("反注册wrapper", function() {
 		var w = function() {};
-		$.regist("number", w);
+		$.regist(w, "number");
 
 		$.unregist("number");
 		expect($.getWrapper("number")).toBeUndefined();
@@ -17,7 +17,7 @@ describe("$()", function() {
 	it("包装值类型", function() {
 		var aw = jasmine.createSpy("aw");
 
-		$.regist("number", aw);
+		$.regist(aw, "number");
 		$(8);
 
 		expect(aw).toHaveBeenCalledWith(8);
@@ -26,11 +26,25 @@ describe("$()", function() {
 	it("包装引用类型", function() {
 		var aw = jasmine.createSpy("aw");
 
-		$.regist(Array, aw);
+		$.regist(aw, Array);
 		var a = [];
 		$(a);
 
 		expect(aw).toHaveBeenCalled();
 	});
-});
 
+	it("同时包装多个接口", function() {
+		var aw = jasmine.createSpy("aw");
+
+		$.regist(aw, [Array, String]);
+
+		var a = [];
+		$(a);
+		expect(aw).toHaveBeenCalled();
+		aw.reset();
+
+		var b = new String("");
+		$(b);
+		expect(aw).toHaveBeenCalled();
+	});
+});
