219e0f5787068eb9e7bc43312a2121e5da1382df
zhongxingdou <zhongxingdou@gmail.com>
2012-05-24 00:54:29 +0800

    util.js增加实用方法和添加注释，更改$option的实现以及更新测试
    增加$getAllMembers,$eachMember,$getProtoMember,$base,$property实用方法
    $option从fn.option获取参数默认值
    同时更新测试

--- test/spec/utilSpec.js
+++ test/spec/utilSpec.js
@@ -1,13 +1,50 @@
 describe("util.js", function() {
+	it("$each将在处理函数返回false时中断", function(){
+		var i;
+		$each([1,2,3], function(n){
+			i = n;
+			if(n===2)return false;
+		});
+		expect(i).toBe(2);
+	});
+
+	it("$each遍历完数组时返回true", function(){
+		var b = $each([1,2,3], function(n){});
+		expect(b).toBe(true);
+	});
+
+	it("$makeArray将fn.arguments转换成Array", function(){
+		function fn (a1,a2) {
+			var args = $makeArray(fn.arguments);
+			return args;
+		}
+
+		var args = fn(1,2);
+		expect(args.constructor).toBe(Array);
+	});
+
+	it("$makeArray从指定位置开始转换", function(){
+		function fn (a1,a2,a3) {
+			var args = $makeArray(fn.arguments, 1);
+			return args;
+		}
+
+		var args = fn(1,2,3);
+		expect(args.length).toBe(2);
+		expect(args[0]).toBe(2);
+		expect(args[1]).toBe(3);
+	});
+
 	it("$option", function() {
 		function fn(p1, p2) {
-			var option = $option({
-				key1: "key1",
-				key2: "key2",
-				key3: "key3"
-			});
-
+			var option = $option();
 			return option;
+
+		}
+		fn.option = {
+			key1: "key1",
+			key2: "key2",
+			key3: "key3"
 		}
 
 		var option = fn("k1", "k2");
@@ -16,7 +53,6 @@ describe("util.js", function() {
 
 		expect(option.key1).toBe("k1");
 		expect(option.key2).toBe("k2");
-
 		expect(option.key3).toBe("key3");
 	});
 
@@ -47,7 +83,7 @@ describe("util.js", function() {
 		expect(b.p2).toBeDefined();
 	});
 
-	it("$mergei不会覆盖已经存在的成员", function(){
+	it("$merge不会覆盖已经存在的成员", function(){
 		var a = {p1: 'pa'};
 		var b = {p1: "pb"};
 
@@ -56,6 +92,100 @@ describe("util.js", function() {
 		expect(b.p1).toBe("pb");
 	});
 
+	it("$getAllMember", function() {
+		var o = {a: 'a', b: 'b'}
+		var ms = $getAllMembers(o)
+		expect(ms).toContain('a');
+		expect(ms).toContain('b');
+	});
+
+	it("$getProtoMember", function(){
+		function A(){};
+		A.prototype = {a1: {}};
+		A.prototype.constructor = A;
+
+		function B(){};
+		B.prototype = new A();
+		B.prototype.b1 = {};
+		B.prototype.a1 = {};
+		B.prototype.constructor = B;
+
+		function C(){};
+		C.prototype = new B();
+		C.prototype.c1 = {};
+		C.prototype.constructor = C;
+
+		var ac = new C();
+		ac.a1 = {};
+
+		expect($getProtoMember(ac, 'a1')).toBe(C.prototype.a1);
+
+		expect($getProtoMember(ac, 'proto.a1')).toBe(B.prototype.a1);
+		expect($getProtoMember(ac, 'proto.a1')).toNotBe(A.prototype.a1);
+
+		expect($getProtoMember(ac, 'proto.proto.a1')).toBe(B.prototype.a1);
+		expect($getProtoMember(ac, 'proto.proto.proto.a1')).toBe(A.prototype.a1);
+	});
+
+	it("$like", function(){
+		var a = {a: 'a', b:'b'};
+		var b = {a: 'a', b:'b'};
+		expect($like(a,b)).toBeTruthy();
+	});
+
+	it("$property", function(){
+		var o = {};
+		$property(o, 'name');
+		expect(o.getName).toBeDefined();
+		expect(o.setName).toBeDefined();
+
+		var name = "Lucy";
+		o.setName(name);
+		expect(o.getName()).toBe(name);
+	});
+
+	it("$base(this)将调用父原型的构造函数", function(){
+		var i = 0;
+		function A(){
+			i++;
+		};
+		A.prototype.constructor = A;
+
+		function B(){
+			$base(this);
+		};
+		B.prototype = new A();
+		B.prototype.constructor = B;
+
+		var ab = new B();
+		expect(i).toBe(2);
+	});
+
+	it("$base(this, action)将调用父原型的方法", function(){
+		var i = 0;
+		function A(){
+		};
+		A.prototype = {action: function(){i++}};
+		A.prototype.constructor = A;
+
+		function B(){
+			$base(this);
+			$base(this,'action');
+		};
+		B.prototype = new A();
+		B.prototype.constructor = B;
+
+		var ab = new B();
+		expect(i).toBe(1);
+	});
+
+	it("$eachMember", function(){
+	
+	});
+
+	it("$eachProto", function(){
+	
+	});
 });
 
 
--- util.js
+++ util.js
@@ -1,4 +1,9 @@
 (function() {
+	/**
+	 * 遍历数组或集合对象
+	 * @param {Array|Object} obj 
+	 * @param {Function} fn(item) 如果fn返回false，将中止遍历
+	 */
 	function $each(obj, fn) {
 		if (!obj) return true;
 		for (var i = 0, l = obj.length; i < l; i++) {
@@ -32,37 +37,6 @@
 	}
 
 	/**
-	 * 将以逗号分隔的参数转换成Option，并合并默认参数
-	 * @example
-	 * function fn(p1, p2){
-	 *     var oDefault = {key1: null, key2: null, key3: true};
-	 *     var option = $option(oDefault, arguments);
-	 *     ....
-	 * }
-	 */
-	function $option(oDefault, args) {
-		var option = {},
-		args = args || $option.caller.arguments,
-		l = args.length;
-
-		if (l == 1) {
-			return $merge(oDefault, args[0]);
-		}
-
-		var k, i = 0;
-		for (k in oDefault) {
-			if (i < l) {
-				option[k] = args[i];
-				i++;
-			} else {
-				option[k] = oDefault[k];
-			}
-		}
-
-		return option;
-	}
-
-	/**
 	 * 如果目标已经存在,则不覆盖
 	 * @example
 	 * function fn(args){
@@ -80,6 +54,41 @@
 		}
 
 		return to;
+	}
+
+
+	/**
+	 * 将以逗号分隔的参数转换成Option，并合并默认参数
+	 * @example
+	 * function fn(p1, p2){
+	 *     var option = $option();
+	 *     ....
+	 * }
+	 * fn("k1", "k2") //option = {key1: "k1", key2: "k2", key3: true}
+	 * fn.option = {key1: null, key2: null, key3: true}
+	 */
+	function $option() {
+		var fn = $option.caller,
+			deft = fn.option, 
+			option = {},
+			args = fn.arguments,
+			l = args.length;
+
+		if (l == 1) { //参数只有一个时，认为它是参数对象，而不是参数数组中的第一个
+			return $merge(deft, args[0]);
+		}else{
+			var k, i = 0;
+			for (k in deft) {
+				if (i < l) {
+					option[k] = args[i];
+					i++;
+				} else {
+					option[k] = deft[k];
+				}
+			}
+		}
+
+		return option;
 	}
 
 
@@ -118,6 +127,9 @@
 		return newo;
 	}
 
+	/**
+	 * 判断两个对象是否相似
+	 */
 	function $like(a, b) {
 		if (a == b) return true;
 
@@ -131,6 +143,113 @@
 		return true;
 	}
 
+
+	/**
+	 * 遍历对象的原型链，从下向上
+	 */
+	function $eachProto(o, fn) {
+		var proto = o.__proto__ || o.constructor.prototype;
+		while (proto) {
+			fn(proto);
+			proto = proto.__proto__;
+		}
+	}
+
+	/**
+	 * 返回对象的所有成员，但不包括原型链中原生原型所包含的成员
+	 */
+	function $getAllMembers(o){
+		var p, members = [];
+		for(p in o){ 
+			members.push(p);
+		}
+		return members;
+	}
+
+	/**
+	 * 遍历对象的所有成员 
+	 */
+	function $eachMember(o, fn){
+		var ar = [];
+		for(p in o){ 
+			ar.push(o[p]);
+		}
+		return $each(ar);
+	}
+
+	/**
+	 * 获取原型链上的成员
+	 */
+	function $getProtoMember(o, name){
+		var proto = o.__proto__ || o.constructor.prototype;
+		if(!proto)return;
+
+		if (name.indexOf("proto.") != - 1) {
+			var uplevel = name.split("proto.").length - 1;
+			var base;
+			for (var i = 1; i < uplevel && proto; i++) {
+				base = proto.__proto__;
+				if(proto === base)break;
+				proto = base;
+			}
+			if (proto) {
+				name = name.slice(name.lastIndexOf(".") + 1);
+			}
+		}
+
+		return proto[name];
+	}
+
+	/**
+	 * 调用父原型的方法
+	 */
+	function $base(o, name, args) {
+		if(arguments.length == 1 || $is(Array, name)){
+			args = name;
+			return $getProtoMember(o, "proto.proto.constructor").apply(o, args);
+		}
+
+		var p = $getProtoMember(o, "proto.proto."+name);
+		if (typeof p != "function") {
+			throw "can't respond to " + '"' + name + '"';
+		}
+		return p.apply(o, args)
+	}
+
+	/**
+	 * 生成属性方法的工厂函数
+	 * @description
+	 * $property(o, 'name', '@RW')
+	 * 将会生成o.getName,o.setName方法，这两个方法将从o.__name设置或访问值
+	 */
+	function $property(o, name, accessor){
+		var args = $option(), 
+			name = args.name, 
+			o = args.scope, 
+			rw = args.accessor;
+
+		var upName = name.slice(0, 1).toUpperCase() + name.slice(1);
+		var privateName = "__" + name;
+		var rw = rw.toUpperCase();
+
+		if (rw.indexOf("R") != - 1) {
+			o["get" + upName] = function() {
+				return o[privateName];
+			};
+		}
+		if (rw.indexOf("W") != - 1) {
+			o["set" + upName] = function(value) {
+				o[privateName] = value;
+			}
+		}
+	}
+
+	$property.option = {
+		scope: undefined,
+		name: undefined, //lowercase
+		accessor: "@RW" //upcase @RW
+	}
+
 	$global("$each", $each);
 	$global("$copy", $copy);
 	$global("$makeArray", $makeArray);
@@ -138,5 +257,10 @@
 	$global("$like", $like);
 	$global("$merge", $merge);
 	$global("$option", $option);
+	$global("$getAllMembers", $getAllMembers);
+	$global("$eachMember", $eachMember);
+	$global("$getProtoMember", $getProtoMember);
+	$global("$base", $base);
+	$global("$property", $property);
 })();
 
