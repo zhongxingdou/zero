97b6a1f2595982737e0b67c96a97b4ec6b629cab
zhongxingdou <zhongxingdou@gmail.com>
2012-06-05 13:37:19 +0800

    修改测试的名称和加入两个新的测试

--- test/spec/classSpec.js
+++ test/spec/classSpec.js
@@ -1,29 +1,38 @@
 describe("$class()", function(){
-	it("未给定任何参数", function(){
+	it("创建类无参数", function(){
 		expect($support(IClass, $class())).toBeTruthy();
 	});
 
-	it("define参数为{}", function(){
-		expect($support(IClass, $class("TestClass", {}))).toBeTruthy();
-	});
-
-	it("未给定define参数", function(){
+	it("创建类只提供类名称", function(){
 		expect($support(IClass, $class("TestClass"))).toBeTruthy();
 	});
 
-	it("只给定了define参数", function(){
-		expect($support(IClass, $class({}))).toBeTruthy();
+	it("创建类define参数为{}", function(){
+		expect($support(IClass, $class("TestClass", {}))).toBeTruthy();
 	});
 
-	it("匿名类", function(){
+	it("创建类只有一个参数且这个参数作为define参数", function(){
+		var define = {
+			prototype: {
+				action: 'action'
+			}
+		}
+		var C = $class(define);
+		expect(C.prototype.action).toBeDefined();
+	});
+
+	it("使用匿名构造函数来创建一个匿名类", function(){
 		var cls = $class(function(){});
 		expect($support(IClass, cls)).toBeTruthy();
 	});
 
-	it("定义类", function(){
+	it("提供完整的参数来创建一个类", function(){
 		function TestClass(){};
 
 		var define = {
+			statics: {
+				instanceCount: 0
+			},
 			properties: {
 				name: "@RW"
 			},
@@ -35,12 +44,11 @@ describe("$class()", function(){
 		$class(TestClass, define);
 
 		expect($support(IClass, TestClass)).toBeTruthy();
-		expect($support(IClass, TestClass)).toBeTruthy();
 
 		//expect(TestClass.name).toBe("TestClass");
 	});
 
-	it("类的原型在外面定义也可以",function(){
+	it("创建类后构造函数的原有prototype没变",function(){
 		function Clazz(){};
 		Clazz.prototype = {a1: {}};
 
@@ -50,7 +58,7 @@ describe("$class()", function(){
 		expect(a.a1).toBeDefined();
 	});
 
-	it("类的静态成中在外面定义也可以", function(){
+	it("创建类后构造函数的原有静态成员还在", function(){
 		function Clazz(){};
 		Clazz.Abc = {a1: {}};
 	
@@ -70,7 +78,7 @@ describe("$class()", function(){
 		expect(Clazz.prototype.m1).toBe(proto.m1);
 	});
 
-	it("子类的实例包含父类原型中的成员", function(){
+	it("子类的实例包含父prototype中的成员,父类的prototype是子类实例的原型之一", function(){
 		function Base(){};
 		$class(Base, {
 			prototype: {
@@ -87,8 +95,14 @@ describe("$class()", function(){
 			prototype: proto
 		});
 
+		var  a = new Clazz();
+
 		expect(Clazz.prototype.baseM1).toBe(Base.prototype.baseM1);
 		expect(Clazz.prototype.m1).toBe(proto.m1);
+		
+		
+		//父类的prototype是子类实例的原型之一
+		expect(Base.prototype.isPrototypeOf(a)).toBeTruthy();
 	});
 
 	it("正确附加staitcs到类", function(){
@@ -101,5 +115,25 @@ describe("$class()", function(){
 		});
 
 		expect(Clazz.init).toBe(statics.init);
+	});
+
+	it("创建的类和它的实例可通过instaceof来验证关系", function(){
+		var C = $class();
+		var ac = new C();
+		expect(ac instanceof C).toBeTruthy();
+	});
+
+	it("验证创建类时提供的prototype是类与其实例的prototype",  function(){
+		var proto = {};
+		var C = $class({prototype: proto});
+		var ac = new C();
+		expect(proto.isPrototypeOf(ac)).toBeTruthy();
+		expect(C.prototype).toBe(proto);
+
+		var fn = function(){};
+		var B = $class(fn, {prototype: proto});
+		var ab = new C();
+		expect(proto.isPrototypeOf(ab)).toBeTruthy();
+		expect(B.prototype).toBe(proto);
 	});
 });
