a81738b8d2e4e05c3a576349302678702b2d4002
zhongxingdou <zhongxingdou@gmail.com>
2012-06-05 13:38:14 +0800

    将$global改成GlobalManager的一个实例，并新加测试代码

--- global.js
+++ global.js
@@ -1,27 +1,24 @@
-(function(host){
-	var IGlobal = {
+(function(host) {
+	var IGlobalMenager = {
 		type: "function(sNames, o)",
 		member: {
 			//注册一个变量为全局变量
-			set: "function(sName, o)", 
+			set: "function(sName, o)",
 
 			//返回一个全局变量
-			get: "function(sName)",
+			get: "function(sName):Object",
 
 			//列举所有的全局变量
-			getAll: "function",
-
-			//反注册一个全局变量
-			unset: "function",
+			list: "function():Array",
 
 			//返回所有被覆盖的变量
-			getOverwrites: "function",
+			getOringinMap: "function():Object",
 
 			//恢复覆盖的变量到原来拥有它的对象上
 			restore: "function(sName)",
 
 			//复制指定全局变量到某个对象上
-			"exportTo": "function(saNames, oTarget)",
+			exportTo: "function(saNames, oTarget)",
 
 			//提供一个沙箱运行环境，确保所执行的代码引用到的是被管理的全局变量
 			run: "function(fn)",
@@ -37,140 +34,159 @@
 		}
 	};
 
-	var __global__ = {};
-	var __origin__ = {};
 
-	function  $global(sName, o){
+
+
+	function GlobalManager(owner, isAutoExport) {
+		var o =  owner;
+		this.getOwner = function(){
+			return o;
+		}
+
+		var isAutoExport = isAutoExport === false ?  false : true;
+
+		this.getIsAutoExport = function(){
+			return isAutoExport;
+		}
+		this.setIsAutoExport = function(v){
+			isAutoExport = v;
+		}
+
+		this.__member = {};
+		this.__origin = {};
+	}
+
+	GlobalManager.prototype = {
+		set: function(sName, o) {
+			if (this.get(sName)) {
+				this.destroy(sName);
+			}
+
+			this.__member[sName] = o;
+			if (this.getIsAutoExport()) {
+				var owner = this.getOwner();
+
+				var old = owner[sName];
+				if (old) {
+					this.__origin[sName] = old; 
+				}
+
+				owner[sName] = this.get(sName);
+			}
+
+			return this;
+		},
+
+		get: function(sName) {
+			return this.__member[sName];
+		},
+
+		list: function() {
+			var a = [];
+			for (var p in this.__member) {
+				a.push(p);
+			}
+			return a;
+		},
+
+		destroy: function(sName) {
+			var m = this.get(sName);
+
+			var owner = this.getOwner();
+			if(owner[sName] === m){
+				try {
+					owner[sName] = null;
+					delete owner[sName];
+				} catch(e) {}
+			}
+			delete this.__member[sName];
+		},
+
+		restore: function(sName) {
+			var o = this.__origin[sName];
+			if (o) {
+				this.getOwner()[sName] = o;
+				delete this.__origin[sName];
+			}
+		},
+
+		getOringinMap: function() {
+			return this.__origin;
+		},
+
+		/**
+		 * 导出指定对象到host对象
+		 * @example
+		 * exportTo("*", window);
+		 * exportTo("$class", window);
+		 * exportTo(["$interface", "$module"], window);
+		 */
+		exportTo: function(sName, oTarget) {
+			if (sName === "*") {
+				var ms = this.__member;
+				var k;
+				for (k in ms) {
+					oTarget[k] = this.get(k);
+				}
+			} else {
+				if (sName instanceof Array) {
+					for (var i = 0, l = sName.length; i < l; i++) {
+						var k = sName[i];
+						oTarget[k] = this.get(k);
+					}
+				} else {
+					oTarget[sName] = this.get(sName);
+				}
+			}
+		},
+
+		run: function(fn) {
+			var aNameList = fn.__args;
+			if (!aNameList) {
+				var ms = fn.toString().match(/\((.*)?\)/);
+				if (ms && ms[1]) {
+					aNameList = ms[1].replace(/ /g, '').split(",");
+				} else {
+					aNameList = [];
+				}
+				fn.__args = aNameList; //cache args
+			}
+
+			if (aNameList.length === 0) {
+				aNameList = this.list();
+				var code = fn.toString();
+				var body = code.substr(code.indexOf("{"));
+
+				//fn = eval("(function(" + aNameList.join(",") + ")" + body + ")"); //throw error in ie
+				fn = eval("(function(){ return function(" + aNameList.join(",") + ")" + body + "})()"); //throw error in ie
+			}
+
+			var oList = [];
+			for (var i = 0, l = aNameList.length; i < l; i++) {
+				oList.push(this.get(aNameList[i]));
+			}
+
+			fn.apply(host, oList);
+		}
+	}
+
+
+	function $global(sName, o) {
 		var self = arguments.callee;
 		self.set(sName, o);
+		return self;
 	}
-
-	$global.set = function(sName, o){
-		if(this.get(sName)){
-			$global.destroy(sName);
-		}
-		
-		__global__[sName] = {value: o, target: null};
-		if(this.autoExport){
-			this.__export(sName, this.defaultTarget);
-		}
-	}
-
-	$global.get = function(sName){
-		var o = __global__[sName];
-		if(o){
-			return o.value;
-		}
-	}
-
-	$global.list = function(){
-		var a = [];
-		for(var p in __global__){
-			a.push(p);
-		}
-		return a;
-	}
-
-	$global.destroy = function(sName){
-		var o = __global__[sName];
-		if(o && o.target){
-			try{
-				o.target[sName] = null;
-				delete o.target[sName];
-			}catch(e){
-			}
-		}
-		delete __global__[sName];
-	}
-
-	$global.restore = function(sName){
-		var o = __origin__[sName];
-		if(o){
-			o.target[sName] = o.value;
-			delete __origin__[sName];
-		}
-	}
-
-	$global.getOverwrites = function(){
-		return __origin__;
-	}
-
-	$global.__export = function(sName, oTarget){
-		if(!oTarget)return;
-		if(oTarget[sName]){
-			__origin__[sName] = {
-				value: oTarget[sName],
-				target: oTarget
-			}
-		}
-
-		var o = __global__[sName];
-		if(o){
-			oTarget[sName] = this.get(sName);
-			o.target = oTarget;
-		}
-	}
-
-	/**
-	 * 导出指定对象到host对象
-	 * @example
-	 * $global.exportTo("*", window);
-	 * $global.exportTo("$class", window);
-	 * $global.exportTo(["$interface", "$module"], window);
-	 */
-	$global.exportTo = function(sNames, oTarget){
-		if(sName === "*"){
-			for(var k in __global__){
-				this.__export(sName, oTarget);
-			}
-		}else{
-			if(sName instanceof Array){
-				for(var i=0,l=sName.length; i<l;  i++){
-					var k =sName[i]; 
-					this.__export(k, oTarget);
-				}
-			}else{
-					this.__export(sName, oTarget);
-			}
-		}
-	}
-
-	$global.run = function(fn){
-		var aNameList = fn.__args;
-		if(!aNameList){
-			var ms = fn.toString().match(/\((.*)?\)/);
-			if(ms && ms[1]){
-				aNameList = ms[1].replace(/ /g, '').split(",");
-			}else{
-				aNameList = [];
-			}
-			fn.__args = aNameList; //cache args
-		}
-
-		if(aNameList.length === 0){
-			aNameList = this.list();
-			var code = fn.toString();
-			var body = code.substr(code.indexOf("{"));
-			
-			//fn = eval("(function(" + aNameList.join(",") + ")" + body + ")"); //throw error in ie
-			fn = eval("(function(){ return function(" + aNameList.join(",") + ")" + body + "})()"); //throw error in ie
-
-		}
-
-		var oList = [];
-		for(var i=0,l=aNameList.length; i<l; i++){
-			oList.push(this.get(aNameList[i]));
-		}
-
-
-		fn.apply(host, oList);
-	}
-
-	var __host__ = null;
 
 	$global.defaultTarget = host;
 
 	$global.autoExport = true;
 
+	var hostMan = new GlobalManager($global.defaultTarget, $global.autoExport);
+	var p;
+	for(p in hostMan){
+		$global[p] = hostMan[p];
+	}
+	
 	$global("$global", $global);
+	$global("GlobalManager", GlobalManager);
 })(this);
---
+++ test/spec/globalSpec.js
@@ -0,0 +1,59 @@
+describe("GlobalManager", function() {
+	var man;
+	var owner;
+	beforeEach(function(){
+		owner = {};
+		man = new GlobalManager(owner);
+	});
+
+	it("测试.set()和.get()", function(){
+		var a = {};
+		man.set('a',a);
+		expect(owner.a).toBe(a);
+		expect(man.get('a')).toBe(a);
+	});
+
+	it(".list()返回所有声明过的对象", function(){
+		man.set("n",8);
+		man.set("k",9);
+
+		var all = man.list();
+		expect(all).toContain('n');
+		expect(all).toContain('k');
+	});
+
+	it(".destroy", function(){
+		var a = {};
+		man.set("a",a);
+
+		man.destroy('a');
+		expect(owner.a).toBeUndefined();
+		expect(man.get('a')).toBeUndefined();
+	});
+
+	it(".restore()恢复变量到原来拥有它的对象上", function(){
+		var old = {};
+		owner.a = old; 
+
+		var b = {};
+		man.set('a', b);
+
+		expect(owner.a).toBe(b);
+
+		man.restore('a');
+
+		expect(owner.a).toBe(old);
+
+		expect(man.get('a')).toBe(b);
+	});
+
+	it(".export()设定到指定对象", function(){
+		var k = {};
+
+		var a = {};
+		man.set("a", a);
+
+		man.exportTo("a", k);
+		expect(k.a).toBe(a);
+	});
+});
