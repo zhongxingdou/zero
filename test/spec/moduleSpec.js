396cf64d37345958e770e7af063f7fee4a28b40c
zhongxingdou <zhongxingdou@gmail.com>
2012-06-26 03:30:43 +0800

    增加mixTo的测试，$include重命名为$mix

--- test/spec/moduleSpec.js
+++ test/spec/moduleSpec.js
@@ -7,50 +7,61 @@ $run(function() {
 			expect($support(IModule, m)).toBeTruthy();
 		});
 
-		it("$include()", function() {
+		it("$mix()", function() {
 			var m = $module({
 				m1: {},
 				m2: {}
 			});
 
 			var o = {};
-			$include(m, o);
+			$mix(m, o);
 
 			expect(o.m1).toBeDefined();
 			expect(o.m2).toBeDefined();
 		});
 
-		it("$include()一个模块时会调用它的onIncluded()", function() {
+		it("$mix()一个模块时会调用它的onIncluded()", function() {
 			var spy = jasmine.createSpy();
 			var m = $module({
 				onIncluded: spy
 			});
 
 			var o = {};
-			$include(m, o);
+			$mix(m, o);
 			expect(spy).toHaveBeenCalled();
 		});
 
-		it("$include()时不会把onIncluded()也放进toObj里去了", function(){
+		it("$mix()时不会把onIncluded()也放进toObj里去了", function(){
 			var m = {
 				onIncluded: function(){}
 			}
 
 			var toObj = {};
-			$include(m, toObj);
+			$mix(m, toObj);
 			expect(toObj.onIncluded).toBeUndefined();
 		});
 
-		it("$include()时调用onIncluded()会传递toObj和option", function(){
+		/*
+		it("$mix()时调用onIncluded()会传递toObj和option", function(){
 			var m = {
 				onIncluded: jasmine.createSpy()
 			}
 
 			var toObj = {};
 			var option = {}
-			$include(m, toObj, option);
+			$mix(m, toObj, option);
 
 			expect(m.onIncluded).toHaveBeenCalledWith(toObj, option);
+		});
+		*/
+
+		it("mixTo(toObj)", function(){
+			var m = {method: function(){}};
+			$module(m);
+
+			var o = {};
+			m.mixTo(o);
+			expect(o.method).toBe(m.method);
 		});
 	});
 });
