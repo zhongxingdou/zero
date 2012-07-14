05bf19ced6f2a89d955f0c096f5fc9db46fa8d4d
zhongxingdou <zhongxingdou@gmail.com>
2012-07-15 07:47:10 +0800

    移除$like,$clone,$getProtoMember,$callWithAll，$hasSubset重命名为$containsAll，增加$array，

--- util.js
+++ util.js
@@ -2,27 +2,33 @@
 	eval($global.all);
 
 	/**
-	 * 遍历数组或集合对象
-	 * @param {Array|Object} obj 
-	 * @param {Function} fn(item) 如果fn返回false，将中止遍历
+	 * 遍历数组或集合对象，处理函数返回false将中断遍历
+	 * @param {Array|Object} items 集合对象
+	 * @param {Function} fn(item) 处理函数
+	 * @param {Object} thisp 让处理函数执行时this指向它
+	 * @return Boolean
 	 */
-	function $every(obj, fn, scope) {
-		if (!obj) return true;
-		for (var i = 0, l = obj.length; i < l; i++) {
-			if (fn.apply(scope, [obj[i]]) === false) return false;
+	function $every(items, fn, thisp) {
+		if (!items) return true;
+		for (var i = 0, l = items.length; i < l; i++) {
+			if (fn.apply(thisp, [items[i]]) === false) return false;
 		}
 		return true;
 	}
 
 
 	/**
-	 * 遍历对象的所有成员 
+	 * 遍历对象的可遍历成员 
+	 * @param {Object} o 对象
+	 * @param {Function} fn(key, value) 处理函数
+	 * @param {Object} thisp 让处理函数执行时this指向它
+	 * @return Boolean
 	 */
-	function $everyKey(o, fn, scope){
+	function $everyKey(o, fn, thisp){
 		if(typeof fn != "function")return false;
 
 		for(p in o){ 
-			if(fn.apply(scope, [p, o[p]]) === false)return false;
+			if(fn.apply(thisp, [p, o[p]]) === false)return false;
 		}
 
 		return true;
@@ -30,12 +36,17 @@
 
 	/**
 	 * 根据指定属性来追溯
+	 * @param {Object} o 对象
+	 * @param {String} prop 属性名
+	 * @param {Function} fn(a) 处理函数(追溯到的对象)
+	 * @param {Object} thisp 让处理函数执行时this指向它
+	 * @return Boolean
 	 */
-	function $trace(o, name, fn, scope){
-		var p = o;
-		while(p){
-			if(fn.apply(scope, [p]) === false)return false;
-			p = p[name];
+	function $trace(o, prop, fn, thisp){
+		var a = o;
+		while(a){
+			if(fn.apply(thisp, [a]) === false)return false;
+			a = a[prop];
 		}
 		return true;
 	}
@@ -44,40 +55,54 @@
 
 	/**
 	 * 遍历对象的原型链，从下向上
+	 * @param {Object} o 对象
+	 * @param {Function} fn(proto) 处理函数(对象原型)
+	 * @param {Object} thisp 让处理函数执行时this指向它
 	 */
 	if(SUPPORTED_PROTO){
-		function $traceProto(o, fn, scope) {
-				return $trace(o.__proto__, '__proto__', fn, scope);
+		function $traceProto(o, fn, thisp) {
+				return $trace(o.__proto__, '__proto__', fn, thisp);
 		}
 	}else{
-		function $traceProto(o, fn, scope) {
+		function $traceProto(o, fn, thisp) {
 			var proto = o.constructor.prototype;
 			if(proto == o){
 				proto = o.constructor.baseProto;
 			}
 
 			while(proto){
-				if(fn.apply(scope, [proto]) === false)return false;
+				if(fn.apply(thisp, [proto]) === false)return false;
 				proto = proto.constructor.baseProto;
 			}
 
 			if(o.constructor !== Object){
-				if(fn.apply(scope, [Object.prototype]) === false)return false;
+				if(fn.apply(thisp, [Object.prototype]) === false)return false;
 			}
 		}
 	}
 
 	/**
-	 * 把集合对象转换成Array
-	 * @param {Object} obj 被转换的对象
-	 * @param {Object} start=0 从集合对象中的第几项开始转换 
+	 * 分割集合对象
+	 * @param {Object} items 集合对象
+	 * @param {Object} start=0 从第几项开始转换 
+	 * @return Array
 	 */
-	function $slice(obj, start) {
-		return Array.prototype.slice.apply(obj, [start || 0]);
+	function $slice(items, start) {
+		return Array.prototype.slice.apply(items, [start || 0]);
 	}
 
 	/**
-	 * copy对象成员到另一个对象
+	 * 将集合对象转换成数组
+	 * @param {Object} items 集合对象
+	 * @return Array
+	 */
+	function $array(items){
+		return Array.prototype.slice.apply(items, 0);
+	}
+
+
+	/**
+	 * 复制对象成员到另一个对象
 	 * @param {Object} from
 	 * @param {Object} to
 	 */
@@ -92,12 +117,9 @@
 	}
 
 	/**
-	 * 如果目标已经存在,则不覆盖
-	 * @example
-	 * function fn(args){
-	 *     var dftOption = {param: true}
-	 *     $merge(dftOption, args);
-	 * }
+	 * 复制对象成员到另一个对象，但对方已有同名成员时不覆盖
+	 * @param {Object} from
+	 * @param {Object} to
 	 */
 	function $merge(from, to) {
 		var p;
@@ -112,40 +134,12 @@
 	}
 
 
-
-
 	/**
-	 * 浅clone
+	 * 获取对象可枚举的所有成员名
+	 * @param {Object} o
+	 * @return Array
 	 */
-	function $clone(obj) {
-		function Clone() { } 
-		Clone.prototype = obj;
-		var c = new Clone();
-		//c.constructor = Clone;
-		return c;
-	}
-
-
-	/**
-	 * 判断两个对象是否相似
-	 */
-	function $like(a, b) {
-		if (a == b) return true;
-
-		if (typeof a != typeof b) return false;
-
-		for (var k in a) {
-			if (a[k] != b[k]) {
-				if (!$like(a[k], b[k])) return false;
-			}
-		}
-		return true;
-	}
-
-	/**
-	 * 返回对象的所有成员，但不包括原型链中原生原型所包含的成员
-	 */
-	function $allKeys(o){
+	function $keys(o){
 		var p, keys = [];
 		for(p in o){ 
 			keys.push(p);
@@ -153,41 +147,45 @@
 		return keys;
 	}
 
-	/**
-	 * 获取原型链上的成员
-	 */
-	function $getProtoMember(o, name){
-		var proto = SUPPORTED_PROTO ? o.__proto__ : o.constructor.prototype;
-		if(!proto)return;
 
-		if (name.indexOf("proto.") != - 1) {
-			var uplevel = name.split("proto.").length - 1;
-			var base;
-			for (var i = 1; i < uplevel && proto; i++) {
-				base = SUPPORTED_PROTO ? proto.__proto__ : (proto.constructor.baseProto || Object.prototype);
-				if(proto === base)break;
-				proto = base;
-			}
-			if (proto) {
-				name = name.slice(name.lastIndexOf(".") + 1);
-			}
-		}
+	function _makeSetter(name){
+		return Function(name+"=arguments[0]");
+	}
 
-		return proto[name];
+	function _makeGetter(name){
+		return Function("return " + name);
 	}
 
 	/**
      * Object.definedProperties的快捷方式
 	 */
 	function $property(){
-		var l = arguments.length;
-		if(l > 2){
-			Object.definedProperties.apply(Object, arguments);
-		}else{
-			Object.definedProperty.apply(Object, arguments);
+		var args = arguments;
+		var l = args.length;
+		var obj = args[0];
+		var t = typeof args[1];
+		if(t == "string"){
+			var name = args[1];
+			var config = args[2] || {};
+			if(!config.set){
+				config.set = _makeSetter(name);
+			}
+			if(!config.get){
+				config.get = _makeGetter(name);
+			}
+			Object.defineProperty(obj, name, config);
+		}else if(t == "object"){
+			$everyKey(args[1], function(name, config){
+				$property(obj, name, config);
+			});
 		}
 	}
 
+	/**
+	 * 用于方法在自己内部调用thisp原型链上的同名方法
+	 * @param {Object} o
+	 * @param {Array} args 参数
+	 */
 	function $callBase(obj, args){
 		var caller = $callBase.caller;
 		//此处不能用caller.name，因为caller.name可能不是它在对象中的key
@@ -216,22 +214,20 @@
 	}
 
 
-	function $callWithAll(fn, args, scope){
-		var isArray = args instanceof Array;
-		if(isArray){
-			return $every(args, fn, scope);
-		}else{
-			return fn.apply(scope, [args]);
-		}
-	}
-
 	/**
 	 * 声明一个枚举
+	 * @param {Object} o
+	 *
 	 * @example
 	 * var color = $enum("BLUE", "RED", "YELLOW");
 	 * color.BLUE
 	 * color.RED
 	 * color.YELLOW
+	 *
+	 * var color = $enum{{
+	 *	"BLUE": -1,
+	 *	"RED": 1
+	 * })
 	 */
 	function $enum(obj){
 		if(arguments.length == 1 && typeof obj == "object"){
@@ -247,9 +243,9 @@
 
 	/***
 	 * 运行一个方法，避免产生全局变量
+	 * @param {Function} fn 要运行的方法
 	 */
 	function $run(fn){
-		"use strict"
 		var thisObj = {};
 		fn.apply(thisObj, arguments);
 		return thisObj;
@@ -258,15 +254,25 @@
 
 	/**
 	 * 判断一个成员的名字是否符合表示私有
+	 * @param {String} name
+	 * @return Boolean
 	 */
 	function $isPrivate(name){
 		return name.toString().match(/^__/);
 	}
 
+	/**
+	 * 在方法内部调用，返回方法自身，避免使用方法名来引用自身
+	 */
 	function $thisFn(){
 		return $thisFn.caller;
 	}
 
+	/**
+	 * 把option添加到指定的fn上
+	 * @param {Function} fn 
+	 * @param {Object} option
+	 */
 	function $fn(fn, option){
 		if(option){
 			fn.option = option;
@@ -274,7 +280,12 @@
 		return fn;
 	}
 
-	function $hasSubset(set, sub) {
+	/**
+	 * 判断一个集合是否包含另一个子集
+	 * @param {Object} set 主集合
+	 * @param {Object} sub 子集合
+	 */
+	function $containsAll(set, sub) {
 		var k,
 			count = 0;
 
@@ -297,23 +308,19 @@
 	var vars = ["$every", "$everyKey", "$trace"];
 
 	//操作与比较
-	vars.push("$copy", "$merge", "$clone", "$like");
+	vars.push("$copy", "$merge");
 
 	//工厂
 	vars.push("$slice","$enum","$property");
 
-	//方法相关
-	vars.push("$callWithAll");
-
 	//反射
-	vars.push("$traceProto", "$callBase",  "$getProtoMember");
+	vars.push("$traceProto", "$callBase");
 
-	vars.push("$isPrivate", "$allKeys", "$thisFn", "$hasSubset");
+	vars.push("$isPrivate", "$keys", "$thisFn", "$containsAll");
 
 	var name;
 	while(name = vars.pop()){
 		$global(name, eval(name));
 	}
-	
 })(this);
 
