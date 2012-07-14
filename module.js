a7a5d501be83a073fd5d220aab078881b0a7a14f
zhongxingdou <zhongxingdou@gmail.com>
2012-07-15 07:38:47 +0800

    $mix重命名为$include，移除module.mixTo，include时不再更新目标对象的implns

--- module.js
+++ module.js
@@ -5,17 +5,17 @@ $run(function(){
 	 * @interface
 	 */
 	var IModule = {
-		onIncluded: "[function()]",
-		mixTo: "[function(toObj)]"
+		onIncluded: "[function()]"
 	};
 
 	var __moduleMember = Object.keys(IModule);
+
 	/**
-	 * mix一个module到toObjObj
+	 * 包含一个module到对象
 	 * @param {IModule} module 
 	 * @param {Object} toObj 
 	 */
-	function $mix(module, toObj) {
+	function $include(module, toObj) {
 		$everyKey(module, function(k, v){
 			if(__moduleMember.indexOf(k) == -1){
 				toObj[k] = v;
@@ -26,26 +26,27 @@ $run(function(){
 			module.onIncluded.call(toObj);
 		}
 
-		if(module.implns){
-			if(!toObj.implns){
-				toObj.implns = [].concat(module.implns);
-			}else{
+		//让调用include的一方来决定是否把module.implns复制过去
+		//if(module.implns){
+			//if(!toObj.implns){
+				//toObj.implns = [].concat(module.implns);
+			//}else{
 				//@todo 去除重复的
-				toObj.implns.concat(module.implns);
-			}
-		}
+				//toObj.implns.concat(module.implns);
+			//}
+		//}
 	}
 
+	/**
+	 * 声明一个模块
+	 * @param {Object} o 模块
+	 */
 	function $module(o){
-		o.mixTo = function(toObj){
-			$mix(o, toObj);
-			return o;
-		}
 		return o;
 	}
 
 	$global("IModule", IModule);
 	$global("$module", $module);
-	$global("$mix", $mix);
+	$global("$include", $include);
 });
 
