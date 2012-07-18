ff8ddd4537a2ef75063380f588b9222235b8e351
zhongxingdou <zhongxingdou@gmail.com>
2012-07-18 15:53:31 +0800

    从$dispatch中拆出解析args部分作为__parseArgs()，重命名部分局部变量，完整注释

--- dispatch.js
+++ dispatch.js
@@ -1,12 +1,22 @@
 $run(function() {
 	eval($global.all);
 
+	/**
+	 * 获取方法的参数数组的个数
+	 * @private
+	 */
 	function __getOptc(fn){
 		var opt= fn.option;
 		return opt ? Object.keys(opt).length : 0;
 	}
 
 
+	/**
+	 * 建立一个参数个数和方法的映射表
+	 * @private
+	 * @param {Function[]} fns 方法数组
+	 * @param {Number} minimum 参数个数的最小值，个数多于此值才进入映射表
+	 */
 	function __makeArgcMap(fns, minimum){
 		var argcMap = {};
 		fns.forEach(function(fn){
@@ -18,6 +28,13 @@ $run(function() {
 		return argcMap;
 	}
 
+	/**
+	 * 验证参数是否符合参数接口定义的规格
+	 * @private
+	 * @param {Interface} option
+	 * @param {Object} args arguments
+	 * @return Boolean
+	 */
 	function __optionMatchArgs(option, args){
 		var	keys = Object.keys(option),
 			i = 0,
@@ -35,55 +52,83 @@ $run(function() {
 		return args.length == i;
 	}
 
+	/**
+	 * 将对象的成员按key出现的顺序装入数组
+	 * @private
+	 * @param {Object} obj
+	 * @return Array
+	 */
 	function __objToArray(obj){
 		var keys = Object.keys(obj);
 		var ar = [];
 		keys.forEach(function(k){
-			ar.push(a0[k]);
+			ar.push(obj[k]);
 		});
 		return ar;
 	}
 
-	function $fnRounter(fns){
+	/**
+	 * 生成一个可路由一组重载方法的方法
+	 * @param {Function[]} fns 方法数组
+	 * @return Function
+	 */
+	function $overwriter(/*fn1, fn2, fn... */){
+		var fns = $array(arguments);
 		return function(){
-			var fn = $dispatch(fns)
+			var fn = $dispatch(fns);
 			fn && fn.apply(this, arguments);
 		}
 	}
 
-	function $dispatch(fns, args){
-		var caller = $thisFn().caller,
-			args = args || caller.arguments,
-			argc = args.length;
-
+	/**
+	 * 解析参数，如果参数为key/value参数，转换成Array
+	 * @private
+	 * @param {Function[]} overfns
+	 * @param {Object} args arguments
+	 */
+	function __parseArgs(overfns, args){
+		var argc = args.length;
 		var a0 = args[0];
+		//如果认定第一个参数为key/value参数，就转换成数组形式，以方便和参数接口进行比较
 		if(argc == 1 && typeof a0 == "object"){
-			if(a0 instanceof Paramap){
+			if(a0 instanceof ArguMap){ 
 				args = __objToArray(a0);
-				argc = args.length;
 			}else{
-				$every(function(fn){
-					opt = fn.option;
+				//只要第一个参数能够符合重载方法中其中一个的参数接口，就认为第一参数为key/value的参数
+				$every(overfns, function(fn){
+					var opt = fn.option;
 					if(opt && $containsAll(Object.keys(opt), Object.keys(a0))){
 						args = __objToArray(a0);
-						argc = args.length;
 						return false; //break the loop
 					}
 				});
 			}
 		}
+		return args;
+	}
 
-		var argcMap = __makeArgcMap(fns, argc);
+	/**
+	 * 根据参数的数量和类型对比方法的参数接口，选择一个方法执行
+	 * @param {Function[]} fns 一组方法
+	 * @param {Object} args arguments
+	 */
+	function $dispatch(overfns, args){
+		var caller = $thisFn().caller,
+			args = __parseArgs(overfns, args || caller.arguments),
+			argc = args.length;
 
-		var keys = Object.keys(argcMap);
-		if(keys.length){
-			keys = keys.sort();
+		//建立参数个数对方法的映射表
+		var argcMap = __makeArgcMap(overfns, argc);
 
-			var k, map_k, fn, opt;
+		var argcNums = Object.keys(argcMap);
+		if(argcNums.length){
+			argcNums = argcNums.sort(); //参数个数从小到大排序
 
-			while(k = keys.shift()){
-				map_k = argcMap[k];
-				while(fn = map_k.shift()){
+			var count, fns, fn, opt;
+
+			while(count = argcNums.shift()){ //参数个数从小到大取出
+				fns = argcMap[count];
+				while(fn = fns.shift()){
 					opt = fn.option;
 					if(!opt || __optionMatchArgs(opt, args)){
 						return fn;
@@ -94,4 +139,5 @@ $run(function() {
 	}
 
 	$global("$dispatch", $dispatch);
+	$global("$overwriter", $overwriter);
 });
