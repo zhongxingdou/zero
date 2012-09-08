5f1fd977572f176ab9fcdcdf18dbc148d462a8e3
zhongxingdou <zhongxingdou@gmail.com>
2012-09-09 02:44:19 +0800

    移除Interface类，改由parseInterface实现;根据util.js的重命名作相应变更。

--- interface.js
+++ interface.js
@@ -2,63 +2,55 @@ $run(function() {
 	eval($global.all);
 
 	/**
+	 * 接口对象的接口
+	 * 定义了instanceOf的时候
+	 */
+	var IInterface = $interface({
+		base: "[object]",
+		member: "[object]",
+		type: z.ITypeSpec,
+		freeze: "[boolean]"
+	});
+
+	/**
 	 * 接口
 	 * @todo 验证数组的所有项都符合成员规格
 	 * @param {Object} member 描述对象的成员
 	 * @param {Object} type 描述对象的类型
 	 */
-	function Interface(member, type) {
-		this.base = null;
-		this.freeze = false;
-		this.type = type || Object; //type可以为Object或Function，如是其它的，可以通过base来指定
-		this.member = member;
+	function parseInterface(member, type) {
+		var o = {};
+		o.base = null;
+		o.freeze = false;
+		o.type = type || Object; //type可以为Object或Function，如是其它的，可以通过base来指定
+		o.member = member;
 
-		this.implement(IInterface);
-		var MemberSpec = z.MemberSpec;
+		// $implement(IInterface, o);
 
 		if(member.member){//说明为hash形式的参数
-			$copy(member, this);
-			this.type = member.type || Object; //确保忽略type参数，此时它应该定义在member.type
+			z._copy(member, o);
+			o.type = member.type || Object; //确保忽略type参数，此时它应该定义在member.type
 		}
 
 		//if(typeof this.type == 'object'){
 			//this.type = $spec(this.type);
 		//}
-		if(this.type){
-			this.type = $spec(this.type);
+		if(o.type){
+			o.type = z.parseTypeSpec(o.type);
 		}
 
 		//将成员的描述实例化成为MemberSpec
-		var p, ms = this.member;
+		var MemberSpec = z.MemberSpec;
+		var p, ms = o.member;
 		for(p in ms){
 			var m = ms[p];
 			if(!(m instanceof MemberSpec)){
 				ms[p] = new MemberSpec(m);
 			}
 		}
+
+		return o;
 	}
-
-
-	Interface.prototype = {
-		/**
-		 * 添加成员描述
-		 * @param {String} name 成员名称
-		 * @param {Object} spec 成员描述
-		 */
-		addMember: function(name, spec){
-			this.member[name] = new z.MemberSpec(spec);
-			return this;
-		},
-		/**
-		 * 添加成员描述
-		 * @param {String} name 成员名称
-		 */
-		removeMember: function(name){
-			delete this.member[name];
-		}
-	}
-
-	$$(Interface).toClass().extend(z.Base);
 
 	/**
 	 * 定义一个接口
@@ -68,31 +60,9 @@ $run(function() {
 	 * @param {Boolean} .freeze = false 是否可以拥有member定义以外的成员
 	 */
 	function $interface(member, type) {
-		if(member instanceof Interface)return member;
-		return new Interface(member, type);
+		var o = parseInterface(member, type);
+		$implement(IInterface, o);
 	}
-
-	$interface(z.IBase);
-
-	$interface(z.ITypeSpec);
-
-	/**
-	 * 接口对象的接口
-	 * 定义了instanceOf的时候
-	 */
-	var IInterface = $interface({
-		base: z.IBase,
-		member: {
-			base: "[object]",
-			member: "[object]",
-			type: z.ITypeSpec,
-			freeze: "[boolean]",
-			addMember: "function(name, spec)",
-			removeMember: "function(name)"
-		},
-		freeze: false,
-		type: "object"
-	});
 
 	/**
 	 * 检测某个对象是否符合接口描述
@@ -114,8 +84,8 @@ $run(function() {
 
 			if (spec.freeze) {
 				var allms = {};
-				$trace(spec, 'base', function(base){
-					$merge(base.member, allms);
+				z._trace(spec, 'base', function(base){
+					z._merge(base.member, allms);
 				});
 
 				for (k in o) {
@@ -128,8 +98,6 @@ $run(function() {
 	}
 
 	z.IInterface = IInterface;
-
-	z.Interface = Interface;
 
 	$global("$interface", $interface);
 
