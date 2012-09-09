d7b020037f805151f1a5a55d15c6981d072394b6
zhongxingdou <zhongxingdou@gmail.com>
2012-09-09 20:58:47 +0800

    简单化option的实现并通过测试，option不再支持多个参数，只支持一个object类型的参数

--- option.js
+++ option.js
@@ -2,108 +2,34 @@ $run(function() {
 	eval($global.all);
 
 	/**
-	 * 将arguments转换成key/value形式的参数，并合并参数的默认值
-	 * 如果只有一个参数，且这个参数不是key/value参数，而是作为第一个参数，请使用$option({key: value})的形式
-	 * 因为在参数少于两个时，不推荐使用$option
-	 * key/value参数，指
-	 * @param {Object} argsmap 手动指定对象作为key/value参数，此参数通常从方法参数获取
-	 * @param {Object} option 手动指定方法的参数的接口，此参数通过从方法的option属性获取
-	 * @example
-	 * function fn(p1, p2){
-	 *     var option = $option();
-	 *     ....
-	 * }
-	 * fn.option = $interface(...);
+	 * 合并参数
+	 * @param {Object} params 手动指定对象作为key/value参数，此参数通常从方法参数获取
+	 * @param {IInterface} paramSpec 手动指定方法的参数的接口，此参数通过从方法的option属性获取
 	 */
-	function $option(/*argsmap, option*/) {
-		var thisArgs = arguments,
-		argsmap      = thisArgs[0],
-		option       = thisArgs[1],
-		thisFn       = $thisFn(),
-		fn           = thisFn.caller,
-		spec         = $interface(option || fn.option),
-		member       = spec.member;
+	function $option(/*params, paramSpec*/) {
+		var args       = arguments,
+			realFn     = $thisFn().caller,
+			params     = args[0] || realFn.arguments[0] || {},
+			paramSpec  = $interface(args[1] || realFn.option);
 
-		if (argsmap){
-			if($support(spec, argsmap)){
-				return __mergeOption(argsmap, member);
-			}else{
-				throw "arguments invalid";
-			}
-		}
-
-		argsmap = argsmap || {};
-
-		/*var args = fn.arguments,
-		argc = args.length,
-		arg0 = args[0];
-		if (argc == 1 && typeof arg0 == 'object') {
-			//if (arg0 instanceof ArgsMap) {//如果第一个参数是一个argsmap，直接认作key/value参数
-				argsmap = arg0; //!!!Keep Simple 只有一个参数就认为是argsmap
-			} else {
-				var arg0Keys = Object.keys(arg0);
-				var deftKeys = Object.keys(member);
-				if (z._containsAll(deftKeys, arg0Keys)) { //如果方法的接口成员包含第一个参数的所有成员，就将第一个参数作为key/value参数
-					argsmap = arg0;
-				} else { //作为第一个参数
-					var key0 = deftKeys[0];
-					argsmap[key0] = arg0;
-				}
-			}
-		} else {//是普通arguments，将它转为argsmap
-			var k, i=0;
-			for(k in member){
-				if(i<argc){
-					argsmap[k] = args[i];
-				}else{
-					break;
-				}
-				i++;
-			}
-		}*/
-
-		if($support(spec, argsmap)){
-			return __mergeOption(argsmap, member);
+		if($support(paramSpec, params)){
+			return __mergeOption(params, paramSpec.member);
 		}else{
 			throw "arguments invalid";
 		}
 	}
 
 
-	function __mergeOption(option, deft) {
+	function __mergeOption(params, deft) {
 		var k;
 		for (k in deft) {
-			if (!(k in option)) {
-				option[k] = deft[k].value;
+			if (!(k in params)) {
+				params[k] = deft[k].value;
 			}
 		}
-		return option;
+		return params;
 	}
 
-	/**
-	 * key/value参数类
-	 * @description 
-	 * 通过情况下并不需要将key/value形式的参数实例化成ArgsMap，
-	 * 仅在有可能将key/value形式的参数误当作普通参数时强制实例化成ArgsMap
-	 */
-	 /*
-	function ArgsMap(argsMap){
-		for(var k in argsMap){
-			this[k] = argsMap[k];
-		}
-	}
-	
-	$$(ArgsMap).toClass().extend(z.Base);
-
-	function $argu(hash){
-		return new ArgsMap(hash);
-	}
-
-
-	z.ArgsMap = ArgsMap;
-
-	$global("$argu", $argu);
-	*/
 
 	$global("$option", $option);
 });
--- test/spec/optionSpec.js
+++ test/spec/optionSpec.js
@@ -13,13 +13,13 @@ $run(function() {
 		});
 
 		it("$option()", function() {
-			var param = [{}, {}]
-			var option = fn(param[0], param[1]);
+			var param = {key1: "p1", key2: "p2"}
+			var option = fn(param);
 
 			expect(option).toBeDefined();
 
-			expect(option.key1).toBe(param[0]);
-			expect(option.key2).toBe(param[1]);
+			expect(option.key1).toBe(param.key1);
+			expect(option.key2).toBe(param.key2);
 		});
 
 		it("只有一个object参数且它的keys是option的keys的子集，会和option合并", function(){
@@ -30,40 +30,21 @@ $run(function() {
 			expect(option.key2).toBe(fn.option.key2.value);
 		});
 
-		it("只有一个object参数但它的keys不是option的keys的子集，会被当作第一个参数", function(){
+		it("只有一个object参数但它的keys不是option的keys的子集，会和option合并", function(){
 			var param = {key3: {}};
 			var option = fn(param);
 
-			expect(option.key1).toBe(param);
-		});
-
-		it("只有一个object参数但它的keys不是option的keys的子集，option的第二个key之后的项会被合并", function(){
-			var param = {key3: {}};
-			var option = fn(param);
-
+			expect(option.key1).toBe(fn.option.key1.value);
 			expect(option.key2).toBe(fn.option.key2.value);
+			expect(option.key3).toBe(param.key3);
 		});
 
-		it("只有一个object参数且它是空对象, 会被当做第一个参数", function(){
+		it("只有一个object参数且它是空对象, 会和option合并", function(){
 			var param = {};
 			var option = fn(param);
 
-			expect(option.key1).toBe(param);
-		});
-
-		it("有两个以上参数时，第一个参数不会被作为key/value参数，即使它是$argu创建的", function(){
-			var param = $argu({key1: {}});
-			var option = fn(param, param);
-
-			expect(option.key1).toBe(param);
-			expect(option.key2).toBe(param);
-		});
-
-		it("只有一个参数时，$argu创建的参数会被当做key/value参数", function(){
-			var key1 = {};
-			var param = $argu({key1: key1});
-			var option = fn(param);
-			expect(option.key1).toBe(key1);
+			expect(option.key1).toBe(fn.option.key1.value);
+			expect(option.key2).toBe(fn.option.key2.value);
 		});
 
 	});
@@ -73,11 +54,13 @@ $run(function() {
 			var fn = function(){
 				return $option();
 			}
+
 			fn.option = {
 				key1 : {type: String}
 			}
 
 			var option = fn();
+
 			expect(option.key1).toBeUndefined();
 		});
 	}); 
