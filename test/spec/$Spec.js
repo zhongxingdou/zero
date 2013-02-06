bfca950279c11a6809a017dcb16a26172a8ba74a
zhongxingdou <zhongxingdou@gmail.com>
2013-02-07 02:03:19 +0800

    添加一些断言

--- test/spec/$Spec.js
+++ test/spec/$Spec.js
@@ -24,50 +24,64 @@ $run(function() {
 		});
 
 		it("包装值类型", function() {
+			var method = function(){};
 			var m = {
-				onIncluded: spy
+				onIncluded: spy,
+				method: method
 			};
 
 			$.regWrapper(m, type, name);
-			$(8, name);
+			var $8 = $(8, name);
+
+			expect($8.method).toBeDefined();
 
 			//expect(spy).toHaveBeenCalledWith(8);
 			expect(spy).toHaveBeenCalled();
 		});
 
 		it("包装引用类型", function() {
+			var method = function(){};
 			type = Array;
 			var m = {
-				onIncluded: spy
+				onIncluded: spy,
+				method: method
 			};
 
 			$.regWrapper(m, type, name);
 
 			var a = [];
-			$(a, name);
+			var $a = $(a, name);
 
+			expect($a.method).toBeDefined();
+			expect(a.method).toBeUndefined();
 			expect(spy).toHaveBeenCalled();
 		});
 
 		it("同时包装多个接口", function() {
+			var method = function(){};
 			type = [Array, String];
 
 			var m = {
-				onIncluded: spy
+				onIncluded: spy,
+				method: method
 			};
 
 			$.regWrapper(m, type, name);
 
 			var a = [];
-			$(a, name);
+			var $a = $(a, name);
 			expect(spy).toHaveBeenCalled();
+			expect($a.method).toBeDefined();
+			expect(a.method).toBeUndefined();
 
 			spy.reset();
 
 			var b = new String("");
-			$(b, "@myWp");
+			var $b = $(b, "@myWp");
 
 			expect(spy).toHaveBeenCalled();
+			expect($b.method).toBeDefined();
+			expect(b.method).toBeUndefined();
 
 			$.removeWrapper(type, name);
 			expect($.getWrapper(Array, name)).toBeUndefined();
